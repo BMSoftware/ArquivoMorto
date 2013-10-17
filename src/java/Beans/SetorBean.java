2bf1e5a633b19ad0c03187c7e5cc63f4c6eea57c
Bruno Alves dos Santos <Bruno@BrunoPC>
2013-10-17 00:25:49 -0300

    Começando a dar problema =D

--- src/java/Beans/SetorBean.java
+++ src/java/Beans/SetorBean.java
@@ -1,9 +1,47 @@
 package Beans;
 
+import Entidade.Setor;
+import RegraDeNegocio.SetorRN;
+import java.util.ArrayList;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.model.SelectItem;
+
 /**
  *
  * @author Bruno
  */
+@ManagedBean(name = "setorBean")
+@RequestScoped
 public class SetorBean {
-    
+
+    private Setor setor = new Setor();
+
+    public String novo() {
+        this.setor = new Setor();
+        return "publico/setor";
+    }
+
+    public String salvar() {
+        SetorRN setorRN = new SetorRN();
+        setorRN.salvar(setor);
+        return "setor";
+    }
+
+    public List<SelectItem> getSetores() {
+        List<SelectItem> lista = new ArrayList<SelectItem>();
+        for (Setor setorAtual : new SetorRN().list()) {
+            lista.add(new SelectItem(setorAtual, setorAtual.getNomeSetor()));
+        }
+        return lista;
+    }
+
+    public Setor getSetor() {
+        return setor;
+    }
+
+    public void setSetor(Setor setor) {
+        this.setor = setor;
+    }
 }
--- src/java/Beans/UsuarioBean.java
+++ src/java/Beans/UsuarioBean.java
@@ -1,10 +1,81 @@
 package Beans;
 
+import Entidade.Setor;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import Entidade.Usuario;
+import RegraDeNegocio.UsuarioRN;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.context.FacesContext;
+
 /**
  *
  * @author Bruno
  */
+@ManagedBean(name = "usuarioBean")
+@RequestScoped
 public class UsuarioBean {
 
-    
+    private Usuario usuario = new Usuario();
+    private String confirmaSenha;
+    @ManagedProperty(value = "#{setorBean}")
+    private SetorBean setorBean;
+    private Setor setor;
+
+    public String novo() {
+        this.usuario = new Usuario();
+        this.usuario.setStatusUsuario(true);
+        return "publico/usuario";
+    }
+
+    public String salvar() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        
+        String senha = this.usuario.getSenhaUsuario();
+        if (!senha.equals(this.confirmaSenha)) {
+            FacesMessage facesMessage = new FacesMessage("A senha não foi confirmada corretamente");
+            context.addMessage(null, facesMessage);
+            return null;
+        }
+        UsuarioRN usuarioRN = new UsuarioRN();
+        usuarioRN.salvar(this.usuario);
+
+        return "usuario";
+    }
+
+    public SetorBean getSetorBean() {
+        return setorBean;
+    }
+
+    public void setSetorBean(SetorBean setorBean) {
+        this.setorBean = setorBean;
+    }
+
+    public Setor getSetor() {
+        if (setorBean != null) {
+            setor = setorBean.getSetor();
+        }
+        return setor;
+    }
+
+    public void setSetor(Setor setor) {
+        this.setor = setor;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getConfirmaSenha() {
+        return confirmaSenha;
+    }
+
+    public void setConfirmaSenha(String confirmaSenha) {
+        this.confirmaSenha = confirmaSenha;
+    }
 }
